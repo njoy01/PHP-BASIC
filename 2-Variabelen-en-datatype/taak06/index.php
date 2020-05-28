8eaeae38a7eb1386b44fda552cbaeb61ef7eec6c
N'Joy Schutte <njoy@MacBook-Air-van-NJoy.local>
2020-05-28 12:57:53 +0200

    taak 6

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,12 @@
+<?php
+$string = "Dit word een string";
+echo gettype($string);
+    $integer = 200321;
+    echo gettype($integer);
+        $float = 790.41;
+        echo gettype($float);
+            $boolean = true;
+            echo gettype($boolean);
+                $null = null;
+                echo gettype($null);
+?>
