ca0f13cabed4b15a3ef8521b66a29d0c9b001fc9
N'Joy Schutte <njoy@MacBook-Air-van-NJoy.local>
2020-06-03 16:00:18 +0200

    taak 1

---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,13 @@
+<?php
+$myMovies = array("Spiderman", "Shrek", "Black panther");
+?>
+
+<ul>
+    <li><?php  echo $myMovies[0];  ?> </li>
+</ul>
+<ul>
+    <li><?php  echo $myMovies[1];  ?> </li>
+</ul>
+<ul>
+    <li><?php  echo $myMovies[2];  ?> </li>
+</ul>
