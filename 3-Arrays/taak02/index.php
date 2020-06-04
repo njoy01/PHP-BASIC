4099f6fc32c8b46ef5de7b93eadb1ced128abf6a
N'Joy Schutte <njoy@MacBook-Air-van-NJoy.local>
2020-06-04 14:49:18 +0200

    taak 2

---
+++ 3-Arrays/taak02/index.php
@@ -0,0 +1,16 @@
+<?php
+$myDogNames = array("johan", "jaap", "peter");
+
+
+echo $myDogNames[0];  //johan
+echo $myDogNames[1];  //jaap
+echo $myDogNames[2];  //peter
+
+
+$myDogNames[1] = 'panter';
+echo $myDogNames[1];  //panter
+
+print_r($myDogNames);
+
+?>
+
