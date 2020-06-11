9e5e36c38c470c19012e2783fd2e4d1eba752228
N'Joy Schutte <njoy@MacBook-Air-van-NJoy.local>
2020-06-11 02:59:48 +0200

    5- taak 2

---
+++ 5-Conditional_Statements/taak01/index.php
@@ -0,0 +1,13 @@
+<?php
+$test1 = 12;
+$test2 = "gewoon tekst";
+
+var_dump($test1 == $test2) . "<br>";
+var_dump($test1 === $test2) . "<br>";
+var_dump($test1 != $test2) . "<br>";
+var_dump($test1 > $test2) . "<br>";
+var_dump($test1 < $test2) . "<br>";
+var_dump($test1 <> $test2) . "<br>";
+var_dump($test1 >= $test2) . "<br>";
+var_dump($test1 <= $test2);
+?>
---
+++ 5-Conditional_Statements/taak02/index.php
@@ -0,0 +1,19 @@
+<?php
+    $test1 = 10;
+    $test2 = 6;
+
+        if($variabele1 == $variabele2) {
+            echo "gelijk" . "<br>"; }
+        if($variabele1 != $variabele2) {
+            echo "ongelijk" . "<br>"; }
+        if($variabele1 === $variabele2) {
+            echo "identiek" . "<br>"; }
+        if($variabele1 > $variabele2) {
+            echo "test1 is groter dan test2" . "<br>"; }
+        if($variabele1 < $variabele2) {
+            echo "test1 is kleiner dan test2" . "<br>"; }
+        if($variabele1 >= $variabele2) {
+            echo "test1 is groter of gelijk aan test2" . "<br>"; }
+        if($variabele1 <= $variabele2) {
+            echo "test1 is kleiner of gelijk aan test2" . "<br>"; }
+?>
